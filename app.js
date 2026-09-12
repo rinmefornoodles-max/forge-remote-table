f542bd12106f59b858bf3b879cfe5acdbb6ef96c
rinmefornoodles-max <rinmefornoodles@gmail.com>
2026-09-12 00:08:20 -0700

    Load expanded Any Card search

--- app.js
+++ app.js
@@ -23,6 +23,7 @@
     './app-src/floating-menu-polish.txt',
     './app-src/top-n-snapshot.txt',
     './app-src/precon-source-fix.txt',
+    './app-src/any-card-search.txt',
     './app-src/part-04.txt'
   ];
 
